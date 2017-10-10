667b3bfa703f09dda82e4799ba42ddf1d450091c
Desana <desana.dax@gmail.com>
2017-10-10 23:17:10 +0200

    Added working project, filled in gaps

---
+++ KSharp/KSharpParser/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace KSharpParser
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            (new Program()).Run();
+        }
+        public void Run()
+        {
+            try
+            {
+                Console.WriteLine("START");
+                RunParser();
+                Console.Write("DONE. Hit RETURN to exit: ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex);
+                Console.Write("Hit RETURN to exit: ");
+            }
+            Console.ReadLine();
+        }
+        private void RunParser()
+        {
+            AntlrInputStream inputStream = new AntlrInputStream("2 * 1");
+            KSharpGrammarLexer lexer = new KSharpGrammarLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            KSharpGrammarParser parser = new KSharpGrammarParser(commonTokenStream);
+            IParseTree tree = parser.expression();
+            IParseTreeVisitor<object> visitor = new KSharpGrammarBaseVisitor<object>();
+            var result = visitor.Visit(tree);
+            Console.WriteLine(tree.ToStringTree(parser));
+        }
+    }
+}
+
